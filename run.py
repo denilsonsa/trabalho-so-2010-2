2c2ce28d06266fc66d7586b20ff6042b24848090
Denilson Sá <denilsonsa@gmail.com>
2010-11-10 15:54:30 -0200

    Script ./run.py agora usa o path absoluto (para evitar possíveis bugs)

--- run.py
+++ run.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 # vi:ts=4 sw=4 et
 
+import os.path
 import subprocess
 
 if __name__ == '__main__':
+    basedir = os.path.abspath(os.path.dirname(__file__))
     webserver = None
 
     try:
         print "Starting webserver"
-        webserver = subprocess.Popen(['./webserver.py'], cwd='./webserver')
+        webserver = subprocess.Popen(
+            ['./webserver.py'],
+            cwd=os.path.join(basedir, 'webserver')
+        )
         webserver.wait()
     except:
     #except KeyboardInterrupt:
