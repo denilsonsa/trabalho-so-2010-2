bcafaa75279e925c41ec845bc0d9afd282d26455
Denilson Sá <denilsonsa@gmail.com>
2010-12-03 18:40:30 -0200

    Script "run.py" inicia os dois servidores (e verifica a versão do Python).

--- run.py
+++ run.py
@@ -2,23 +2,42 @@
 # -*- coding: utf-8 -*-
 # vi:ts=4 sw=4 et
 
+import sys
 import os.path
 import subprocess
 
 if __name__ == '__main__':
+
+    if sys.version_info[0] != 2:
+        print "AVISO! Este trabalho requer Python 2.6"
+        print "AVISO! Este trabalho não funcionará no Python 3.x"
+        print "Tentando rodar mesmo assim..."
+
     basedir = os.path.abspath(os.path.dirname(__file__))
     webserver = None
+    dbserver = None
 
     try:
+        print "Starting dbserver"
+        dbserver = subprocess.Popen(
+            ['java', 'br.ufrj.dcc.so.cinema.ListenerThread'],
+            cwd=os.path.join(basedir, 'dbserver')
+        )
+
         print "Starting webserver"
         webserver = subprocess.Popen(
-            ['./webserver.py'],
+            ['python', 'webserver.py'],
             cwd=os.path.join(basedir, 'webserver')
         )
+
+        dbserver.wait()
         webserver.wait()
-    except:
-    #except KeyboardInterrupt:
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if dbserver:
+            print "Terminating dbserver"
+            dbserver.terminate()
         if webserver:
             print "Terminating webserver"
             webserver.terminate()
-
